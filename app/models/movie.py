0d5bf0eaf3282c4e01b5e36255c1318e738d8852
atienoobwanda <atienoobwanda@gmail.com>
2022-04-25 17:15:20 +0300

    Define movie class as well as movie test

---
+++ app/models/movie.py
@@ -0,0 +1,13 @@
+class Movie:
+    '''
+    Movie class to define Movie object
+    '''
+
+    def __init__(self, id, title,overview, poster,vote_average, vote_count):
+        self.id = id
+        self.title = title
+        self.overview = overview
+        self.poster = poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+        
