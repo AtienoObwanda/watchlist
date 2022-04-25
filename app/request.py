2dc3ce0ceb408900cd7709aed470424de93dc486
atienoobwanda <atienoobwanda@gmail.com>
2022-04-25 17:23:55 +0300

    importing the request base url into the request file, moving the movi_test file from models folder to the app folder

--- app/request.py
+++ app/request.py
@@ -1,6 +1,15 @@
+import json
 from app import app
+import urllib.request,json 
+from .models import movie
+
+Movie= movie.Movie
+
 #Getting the api key
 api_key = app.config['MOVIE_API_KEY']
 
-def get_movies(category):
-    pass
+#Getting movie base url 
+base_url = app.config["MOVIE_BASE_URL"]
+
+# def get_movies(category):
+#     pass
