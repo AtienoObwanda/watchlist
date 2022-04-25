64f39daed0fab50697b8ef183dd70cf2ae3e5615
atienoobwanda <atienoobwanda@gmail.com>
2022-04-25 14:30:08 +0300

    adding dynamic routes

--- app/views.py
+++ app/views.py
@@ -4,7 +4,17 @@ from app import app
 #Views
 @app.route('/')
 def index():
+
     '''
     View root page function that returns index page and it's data
     '''
-    return render_template('index.html')
+
+    message = 'Hello Mish'
+    return render_template('index.html', message=message)
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+    '''
+    View movie page function that returns index page and it's data'
+    '''
+    return render_template('movie.html',id=movie_id)
