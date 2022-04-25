05bc6810a5d32db39fb75b369fa43ab0385bed52
atienoobwanda <atienoobwanda@gmail.com>
2022-04-25 17:47:01 +0300

    addding API requests

--- app/request.py
+++ app/request.py
@@ -11,5 +11,49 @@ api_key = app.config['MOVIE_API_KEY']
 #Getting movie base url 
 base_url = app.config["MOVIE_BASE_URL"]
 
-# def get_movies(category):
-#     pass
+def get_movies(category):
+    '''
+    Function that gets the json response from the url request
+    '''
+    get_movies_url= base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results = None
+
+        if get_movies_response['results']:
+            movie_results_list=get_movies_response['results']
+            movie_results= process_results(movie_results_list)
+
+            return movie_results 
+    
+    def process_results(movie_list):
+        '''
+        Function that processes the movie results and transform them to a list of objects
+        
+        Args:
+        movie_list: A list of dictionaries that contain movie details
+
+        Returns :
+        movie_results: A list of movie objects
+        '''
+
+        movie_results = []
+        for movie_item in movie_list:
+            id = movie_item.get['id']
+            title = movie_item.get['title']
+            overview = movie_item.get['overview']
+            poster = movie_item.get['poster']
+            vote_average = movie_item.get['vote_average']
+            vote_count = movie_item.get['vote_count']
+
+            if poster:
+                movie_object= Movie(id, title, overview, poster,vote_average, vote_count)
+                
+                movie_results.append(movie_object)
+                
+            return movie_results
+
+    
