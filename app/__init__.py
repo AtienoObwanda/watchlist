360714a9a18e4c0e695923c0ebf739303b38cf2e
atienoobwanda <atienoobwanda@gmail.com>
2022-04-27 02:37:20 +0300

    initializing bootstrap by passing in the app instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
+from ensurepip import bootstrap
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__,instance_relative_config=True)
@@ -8,5 +10,7 @@ app = Flask(__name__,instance_relative_config=True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+#Initializingg Flask extension
+bootstrap = Bootstrap(app)
 
 from app import views
