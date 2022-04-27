c2bae8eaee91357b9273f50a63902526c38be988
atienoobwanda <atienoobwanda@gmail.com>
2022-04-27 15:03:47 +0300

    adjusting import from  "from wtforms.validators import Required" to "from wtforms.validators import InputRequired" since the first one is depricated

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms.validators import InputRequired
 
 class ReviewForm(FlaskForm):
 
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
+    title = StringField('Review title',validators=[InputRequired()])
+    review = TextAreaField('Movie review', validators=[InputRequired()])
     submit = SubmitField('Submit')
