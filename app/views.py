8860bd9e74e0f4ea5716bd708b3dc63d8f6d14ef
atienoobwanda <atienoobwanda@gmail.com>
2022-04-27 14:41:45 +0300

    Updating the movie view function and call the get_reviews class method that takes in a movie ID and will return a list of reviews for that movie

--- app/views.py
+++ app/views.py
@@ -44,9 +44,9 @@ def movie(id):
 
     movie = get_movie(id)
     title = f'{movie.title}'
-    # reviews = Review.get_reviews(movie.id)
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie) #reviews = reviews
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews) #
 
 
 
