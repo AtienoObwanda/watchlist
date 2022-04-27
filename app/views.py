31e3ed37a332f716211e56598ba045ed9f6fa6a2
atienoobwanda <atienoobwanda@gmail.com>
2022-04-27 14:34:56 +0300

    adding dynamic url for the form

--- app/views.py
+++ app/views.py
@@ -3,7 +3,10 @@ from app import app
 from .request import get_movies, get_movie, search_movie
 from flask import render_template,redirect,url_for
 from flask import request
+from .models import review
+from .forms import ReviewForm
 
+Review = review.Review
 
 #Views
 @app.route('/')
@@ -57,5 +60,19 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
 
-
     return render_template('search.html',movies = searched_movies, title = title)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
