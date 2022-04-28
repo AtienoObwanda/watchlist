1e45953b74989e040e18bd80184491bfaddf4208
atienoobwanda <atienoobwanda@gmail.com>
2022-04-28 08:04:04 +0300

    styling the search

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ class Config:
     '''
     
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY= os.environ.get('MOVIE_API_')
-    SECRET_KEY=os.environ.get('MOVIE_SECRET_')
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 class ProdConfig(Config):
     '''
@@ -27,7 +28,7 @@ class DevConfig(Config):
 
     DEBUG = True
 
-config_options={ #dictionary of config options to help us access different configuration option classes
-    'development' : DevConfig,
-    'production' : ProdConfig
+config_options = { #dictionary of config options to help us access different configuration option classes
+    'development':DevConfig,
+    'production':ProdConfig,
 }
