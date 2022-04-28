715f71c39e2435d1504d231df443deeac3bd5603
atienoobwanda <atienoobwanda@gmail.com>
2022-04-29 01:41:40 +0300

    First Heroku

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app
 from flask_script import Manager, Server
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server', Server)
