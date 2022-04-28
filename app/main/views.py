43f0ec129e11537d9bb4e441cea80c0d5af32bf3
atienoobwanda <atienoobwanda@gmail.com>
2022-04-28 06:06:24 +0300

    Updating main/views.py file

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,12 @@
-from flask import render_template
-from app import app
+from flask import render_template,redirect,url_for, request
+from . import main
 from ..request import get_movies, get_movie, search_movie
-from flask import render_template,redirect,url_for
-from flask import request
-from ..models import review
+from ..models import Review
 from .forms import ReviewForm
 
-Review = review.Review
 
 #Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -26,21 +23,17 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('main.index')('search',movie_name=search_movie))
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
    
 
-@app.route('/movie/<int:id>')
-# @app.route('/movie/<movie_id>')
+@main.route('/movie/<int:id>')
 
-# def movie(movie_id):
 def movie(id):
     '''
     View movie page function that returns index page and it's data'
     '''
-    # title = f'Currently viewing {movie_id}'
-    # return render_template('movie.html', title=title)
 
     movie = get_movie(id)
     title = f'{movie.title}'
@@ -50,7 +43,7 @@ def movie(id):
 
 
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -62,7 +55,7 @@ def search(movie_name):
 
     return render_template('search.html',movies = searched_movies, title = title)
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
@@ -72,7 +65,7 @@ def new_review(id):
         review = form.review.data
         new_review = Review(movie.id,title,movie.poster,review)
         new_review.save_review()
-        return redirect(url_for('movie',id = movie.id ))
+        return redirect(url_for('main.index')('movie',id = movie.id ))
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
