d6e4cdfd83acd64dbd00f4d7a7c10f110d8534c2
atienoobwanda <atienoobwanda@gmail.com>
2022-05-03 07:29:11 +0300

    adding db

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from ensurepip import bootstrap
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
-#bootstrap
+
+#imports
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name): # function that takes the configuration setting key as an argument
     app = Flask(__name__)
@@ -14,6 +17,7 @@ def create_app(config_name): # function that takes the configuration setting key
 
     # Initializing flask extension
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Registering blueprint
     from .main import main as main_blueprint
