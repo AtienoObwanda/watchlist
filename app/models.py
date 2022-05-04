6b7c8a3b24ccf2b343c1fef08e6ff4f62932e482
atienoobwanda <atienoobwanda@gmail.com>
2022-05-04 11:31:22 +0300

    hashing passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 class Movie:
     '''
@@ -54,9 +56,14 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
