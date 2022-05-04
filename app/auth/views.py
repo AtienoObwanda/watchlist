4e80f375406be26edecd81c4dfaf621032dcd942
atienoobwanda <atienoobwanda@gmail.com>
2022-05-04 13:39:06 +0300

    updating login routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,26 @@
-from flask import render_template,redirect,url_for
+from turtle import title
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
 
 from . import auth
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Login unsuccessful! | Invalid Username or Password')
+
+        title="Watchlist Login"
+
+    return render_template('auth/login.html', login_form = login_form, title=title)
 
 
 @auth.route('/register', methods = ['GET','POST'])
@@ -18,7 +31,10 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
-        title = "New account"
-        
-    return render_template('auth/register.html',registration_form=form)
+
+    title="New account"
+
+    return render_template('auth/register.html',registration_form=form, title = title)
+
+
 
