a05d15d9317f93b44f647506d3fe656f9f9061f9
atienoobwanda <atienoobwanda@gmail.com>
2022-05-04 13:03:47 +0300

    adding forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,6 @@
+# from django.forms import ValidationError
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField, ValidationError
 from wtforms.validators import InputRequired, Email,EqualTo
 from .. models import User
 
@@ -7,9 +8,18 @@ from .. models import User
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your email address',validators=[InputRequired(),Email()])
+    def validate_email(self,data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('There is already a registration with that email address')
+    
     username = StringField('Your username',validators=[InputRequired()])
+    def validate_username(self,data_field):
+        if User.query.filter(username=data_field.data).first():
+            raise ValidationError('There is already a registration with that username')
+    
     password = PasswordField('Your password',validators=[InputRequired(),
             EqualTo('password_confirm', message = 'Passwords must match')])
+
     password_confirm = PasswordField('Confirm Password',validators=[InputRequired()])
-    submit = SubmitField('Sign Up')
     
+    submit = SubmitField('Sign Up')
