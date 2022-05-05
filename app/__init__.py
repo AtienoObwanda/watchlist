bb4a7c9f77c4cd672cc334d3b3319ee144505543
atienoobwanda <atienoobwanda@gmail.com>
2022-05-05 12:57:36 +0300

    adding upload form

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong' # attribute provides different security levels and by setting it to strong will monitor the changes in a user's request header and log the user out.
@@ -12,6 +15,8 @@ login_manager.login_view = 'auth.login'
 #imports
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+photos = UploadSet('photos', IMAGES)
 
 def create_app(config_name): # function that takes the configuration setting key as an argument
     app = Flask(__name__)
@@ -32,8 +37,11 @@ def create_app(config_name): # function that takes the configuration setting key
     from .request import configure_request
     configure_request(app)
     
-#blueprint instance
+    # blueprint instance
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate') # argument that will add a prefix to all the routes registered with that blueprint. 
 
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
     return app
--- config.py
+++ config.py
@@ -9,6 +9,17 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://atieno:mishi@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # SUBJECT_PREFIX = 'Watchlist'
+    # SENDER_EMAIL = 'obwandaa@gmail.com'
 
 class ProdConfig(Config):
     '''
