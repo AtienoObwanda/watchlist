72fcbcc0c3a43b6057342527990203f6acf50ba0
atienoobwanda <atienoobwanda@gmail.com>
2022-05-05 13:02:36 +0300

    creating a route that processes photos upload

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from ..models import Review, User
 from .forms import ReviewForm, UpdateProfile
 from . import main
 from flask_login import login_required
-from .. import db
+from .. import db,photos
 
 
 #Views
@@ -98,3 +98,15 @@ def update_profile(uname):
         return redirect(url_for('.profile', uname = user.username))
 
     return render_template('profile/update.html', form=form)
+
+
+@main.route('/user/<uname>/update/pic', methods=['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter(User.username == uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
